adc89a59fd6a97107f5db36a659ada6d1799f1f5
rgsad <rsad3262@gmail.com>
2025-02-16 16:46:11 +0300

    final tasks solution

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        double fahrenheit = 9./5. * temperatureCelsius + 32;
+        System.out.println(fahrenheit);
     }
 }
